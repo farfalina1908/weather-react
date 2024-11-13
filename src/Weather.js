81017e0412b89e74fc7d667a95395eb0f9dc6402
Elena Berezina <farfalina1908@gmail.com>
2024-11-13 14:52:12 +0200

    Added axios

---
+++ src/Weather.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { Audio } from 'react-loader-spinner'
+
+export default function Weather(props) {
+
+    function handleResponse(response) {
+       
+alert(`The weather in ${props.city} is ${response.data.main.temp}C`);
+
+    } 
+        let apiKey = "094780c710fa4efd669f0df8c3991927";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse)
+        return (
+            <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+        )
+
+
+   
+}
